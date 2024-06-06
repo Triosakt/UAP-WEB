347dc84121e66b69ac95bbbc62746660f4655dc3
fajaralfad <fajaralfad85@gmail.com>
2024-06-06 20:04:53 +0700

    perubahan pada file2 tersebut

--- edit-kategori.php
+++ edit-kategori.php
@@ -72,9 +72,6 @@
 			<small>&copy; 2024 PSphere. All Rights Reserved.</small>
 		</div>
 	</footer>
-
-	<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
 </html>
