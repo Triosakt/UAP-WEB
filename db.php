ca211f7f9928b0d99a24d9edbd9f89009630e374
fajaralfad <fajaralfad85@gmail.com>
2024-06-06 18:43:38 +0700

    perubahan pada file database

--- db.php
+++ db.php
@@ -2,7 +2,7 @@
 	$hostname = 'localhost';
 	$username = 'root';
 	$password = '';
-	$dbname   = 'db_bukawarung';
+	$dbname   = 'triorental';
 
 	$conn = mysqli_connect($hostname, $username, $password, $dbname) or die ('Gagal terhubung ke database');
 ?>
